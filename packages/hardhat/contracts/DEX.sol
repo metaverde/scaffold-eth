41d017ed7eed0d355dce3980446427bc20fc05fb
metaverde <jenniferleesilves@gmail.com>
2022-02-14 11:38:16 -0800

    Undid Ownable. It's a DEX. Duhr.

--- packages/hardhat/contracts/DEX.sol
+++ packages/hardhat/contracts/DEX.sol
@@ -5,9 +5,8 @@ import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DEX is Ownable {
+contract DEX {
 
   using SafeMath for uint256;
   IERC20 token;
