50f5234e9db55c6ff7d0458da16ca0159afdd0d3
metaverde <jenniferleesilves@gmail.com>
2022-02-15 15:52:35 -0800

    Added functionality to contract.

--- packages/hardhat/contracts/DEX.sol
+++ packages/hardhat/contracts/DEX.sol
@@ -25,5 +25,60 @@ contract DEX {
     require(token.transferFrom(msg.sender, address(this), tokens));
     return totalLiquidity;
   }
+  function price(uint256 input_amount, uint256 input_reserve, uint256 output_reserve) public view returns (uint256) {
+    uint256 input_amount_with_fee = input_amount.mul(997);
+    uint256 numerator = input_amount_with_fee.mul(output_reserve);
+    uint256 denominator = input_reserve.mul(1000).add(input_amount_with_fee);
+    return numerator / denominator;
+  }
 
+  function ethToToken() public payable returns (uint256) {
+    uint256 token_reserve = token.balanceOf(address(this));
+    uint256 tokens_bought = price(msg.value, address(this).balance.sub(msg.value), token_reserve);
+    require(token.transfer(msg.sender, tokens_bought));
+    return tokens_bought;
+  }
+  function tokenToEth(uint256 tokens) public returns (uint256) {
+    uint256 token_reserve = token.balanceOf(address(this));
+    uint256 eth_bought = price(tokens, token_reserve, address(this).balance);
+    payable(msg.sender).transfer(eth_bought);
+    require(token.transferFrom(msg.sender, address(this), tokens));
+    return eth_bought;
+  }
+
+  function deposit() public payable returns (uint256) {
+    uint256 eth_reserve = address(this).balance.sub(msg.value);
+    uint256 token_reserve = token.balanceOf(address(this));
+    uint256 token_amount = (msg.value.mul(token_reserve) / eth_reserve).add(1);
+    uint256 liquidity_minted = msg.value.mul(totalLiquidity) / eth_reserve;
+    liquidity[msg.sender] = liquidity[msg.sender].add(liquidity_minted);
+    totalLiquidity = totalLiquidity.add(liquidity_minted);
+    require(token.transferFrom(msg.sender, address(this), token_amount));
+    return liquidity_minted;
+  }
+  // function withdraw(uint256 amount) public returns (uint256, uint256) {
+  //   uint256 token_reserve = token.balanceOf(address(this));
+  //   uint256 eth_amount = amount.mul(address(this).balance) / totalLiquidity;
+  //   uint256 token_amount = amount.mul(token_reserve) / totalLiquidity;
+  //   liquidity[msg.sender] = liquidity[msg.sender].sub(eth_amount);
+  //   totalLiquidity = totalLiquidity.sub(eth_amount);
+  //   payable(msg.sender).transfer(eth_amount);
+  //   require(token.transfer(msg.sender, token_amount));
+  //   return (eth_amount, token_amount);
+  // }
+
+  function withdraw(uint256 _amount) public returns (uint256, uint256) {
+        uint256 tokenReserve = token.balanceOf(address(this));
+        uint256 ethAmount = _amount.mul(address(this).balance) / totalLiquidity;
+        uint256 tokenAmount = _amount.mul(tokenReserve) / totalLiquidity;
+        
+// I made changes to below two lines. Instead of eth_amount I used liquidity amount requested by sender.
+        liquidity[msg.sender] = liquidity[msg.sender].sub(_amount);
+        totalLiquidity = totalLiquidity.sub(_amount);
+      
+  (bool sent, ) = payable(msg.sender).call{value: ethAmount}("");
+        require(sent, "Eth transfer failed");
+        require(token.transfer(msg.sender, tokenAmount));
+        return (ethAmount, tokenAmount);
+    }
 }
